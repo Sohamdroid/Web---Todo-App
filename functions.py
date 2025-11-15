b93ced528dce2b86f978fdbdf8c181009494df7c
Soham Pawar <soham.pawar2@gmail.com>
2025-11-15 20:11:12 +0000

    Initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return [t.strip() for t in todos]  # remove ALL whitespace
+
+def write_todos(todos, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(t + "\n" for t in todos)
+
+print("Hello")
